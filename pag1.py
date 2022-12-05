10c0b456b1a9b0721b8550327bf705ebe41e527e
Frenz86 <frenzy86@gmail.com>
2022-12-05 19:35:08 +0100

    add prophet pag1

--- pag1.py
+++ pag1.py
@@ -3,17 +3,48 @@ import plotly.figure_factory as ff
 from PIL import Image
 import pandas as pd
 import numpy as np
-
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from datetime import date
+import joblib 
 
 def show_footer():
     st.markdown("***")
     st.markdown("**Like this infomanager tool?** Follow me on "
                 "[Twitter](https://twitter.com/xxxxxx).")
 def main():
-    st.button("Re-run")
-    st.title("Welcome page1")
-    st.markdown("***")
-    show_footer()
+    st.title('Prophet Test')
+
+    url ="https://frenzy86.s3.eu-west-2.amazonaws.com/timeseries/Data/airline_passengers.csv"
+
+    df = pd.read_csv(url)
+    st.dataframe(df)
+
+    df.columns = ['ds','y']
+    df['ds'] = pd.to_datetime(df['ds'])
+
+    model = joblib.load('model_prophet.pkl')
+    model.fit(df)
+
+    future = model.make_future_dataframe(50, freq='MS')
+
+    #prediction
+    forecast = model.predict(future)
+
+    #st.dataframe(forecast[['ds','yhat']])
+
+
+    fig = plot_plotly(model, forecast)
+
+    fig.update_layout(title="Forecast Air Passengers",
+                    yaxis_title='Air Passengers',
+                    xaxis_title="Date",
+                    )
+    #################### vertical line ############
+    #fig.add_vline(x=date.today(), line_width=3, line_dash="dash", line_color="red")
+
+    ##############################################
+    st.plotly_chart(fig)
 
 if __name__ == "__main__":
     main()
